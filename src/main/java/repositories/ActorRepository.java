23f5a907cfffb47b7a008ce26803503b169c310e
yacinty <yacinty@users.noreply.github.com>
2018-02-24 12:10:35 +0100

    Merge branch 'master' of /Users/yuzi/WorkSpace_IntelliJGit_github_private2/Acme-Rendezvous with conflicts.

--- src/main/java/repositories/ActorRepository.java
+++ src/main/java/repositories/ActorRepository.java
@@ -10,4 +10,5 @@ public interface ActorRepository extends JpaRepository<Actor, Integer>{
 
     @Query("select a from Actor a where a.userAccount.id = ?1")
     Actor findByUserAccountId(int userAccountId);
+
 }
